a12af5a2b3558a1b07c6a897501deeed62db3840
Jonathan Ginsburg <a01021617@itesm.mx>
2018-01-18 01:48:19 -0600

    Add order statistic tree project.

---
+++ deliverables/01-order_statistic_tree/main.js
@@ -0,0 +1,12 @@
+// See https://en.wikipedia.org/wiki/Order_statistic_tree.
+// The following implements an order statistic tree class.
+
+var OSTPrototype = {
+
+};
+
+function OST() {
+	
+}
+
+OST.prototype = OSTPrototype;
